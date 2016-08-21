260202af887f9cf34f1417fcffa5f7dee9f0e979
Ariel Coppes <ariel.coppes@gmail.com>
2016-08-21 13:35:14 -0300

    started tdd for a deterministic game with lockstep simulation

---
+++ Assets/Editor/TestGameStep.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+public class TestGameStep {
+
+	[Test]
+	public void TestFixedGameStepTwoUpdates()
+	{
+		GameStepCode gameStepCode = new GameStepCode ();
+
+		gameStepCode.FixedTimeStepMilliseconds = 50;
+
+		gameStepCode.Init ();
+
+		gameStepCode.Update (10);
+
+		Assert.That (gameStepCode.CurrentUpdate, Is.EqualTo (0));
+
+		gameStepCode.Update (40);
+
+		Assert.That (gameStepCode.CurrentUpdate, Is.EqualTo (1));
+	}
+
+	[Test]
+	public void TestFixedGameStepGreaterThanFixedTime()
+	{
+		GameStepCode gameStepCode = new GameStepCode ();
+
+		gameStepCode.FixedTimeStepMilliseconds = 50;
+
+		gameStepCode.Init ();
+
+		gameStepCode.Update (51);
+
+		Assert.That (gameStepCode.CurrentUpdate, Is.EqualTo (1));
+	}
+
+}
