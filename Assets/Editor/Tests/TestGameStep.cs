9764b0f80f225eccd1162b6242d788b66a2f150a
Ariel Coppes <ariel.coppes@gmail.com>
2016-08-26 15:20:55 -0300

    added nsubstitute, added first lockstep logic

--- Assets/Editor/Tests/TestGameStep.cs
+++ Assets/Editor/Tests/TestGameStep.cs
@@ -111,4 +111,30 @@ public class TestGameStep {
 		Assert.That (lockstepImplementation.CurrentLockstepFrame, Is.EqualTo (2));
 	}
 
+	[Test]
+	public void LockstepTurnShouldNotAdvanceIfWaitingForActions(){
+
+//		var gameLogic = NSubstitute.Substitute.For<DeterministicGameLogic> ();
+
+		var gameLogic = new GameStepEngineMock ();
+
+		CommandsList pendingCommands = new CommandsList ();
+		pendingCommands.IsReady = false;
+
+		LockstepGameLogic lockstepGameLogic = new LockstepGameLogic (gameLogic, pendingCommands);
+
+		lockstepGameLogic.GameFramesPerLockstep = 1;
+
+		lockstepGameLogic.Update (0.1f, 0);
+		lockstepGameLogic.Update (0.1f, 1);
+		lockstepGameLogic.Update (0.1f, 2);
+
+		Assert.That (gameLogic.lastFrame, Is.EqualTo (0));
+
+		pendingCommands.IsReady = true;
+
+		lockstepGameLogic.Update (0.1f, 3);
+
+		Assert.That (gameLogic.lastFrame, Is.EqualTo (1));
+	}
 }
