39e0e8d78187518d251f7a7abdbc13e6dc2c4357
Ariel Coppes <ariel.coppes@gmail.com>
2016-09-11 12:43:25 -0300

    added network sample for blogpost

---
+++ Assets/Scenes/NetworkingSample/NetworkSampleMovement.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkSampleMovement : NetworkBehaviour {
+
+	Vector2 _gamePosition;
+
+	Vector2 _destination;
+
+	public float speed = 1.0f;
+
+	bool _moving = false;
+
+	void Awake()
+	{
+		_gamePosition = transform.position;
+		_destination = _gamePosition;
+		_moving = false;
+	}
+
+	public void SetPosition(Vector2 position)
+	{
+		transform.position = position;
+
+		_gamePosition = transform.position;
+
+		_destination = _gamePosition;
+		_moving = false;
+	}
+
+	public void MoveTo(Vector2 destination)
+	{
+		_destination = destination;
+		_moving = true;
+	}
+
+	[ServerCallback]
+	public void Update()
+	{
+		if (!_moving)
+			return;
+
+		Vector2 direction = (_destination - _gamePosition).normalized;
+
+		float realSpeed = speed * Time.deltaTime;
+
+		Vector2 newPosition = _gamePosition + direction * realSpeed;
+
+		if ((_destination - newPosition).SqrMagnitude() < realSpeed * realSpeed) {
+			//			newPosition = _destination;
+			_moving = false;
+		}
+
+		_gamePosition = newPosition;
+	}
+
+	[ServerCallback]
+	void LateUpdate()
+	{
+		transform.position = _gamePosition;
+	}
+
+}
---
+++ Assets/Scenes/NetworkingSample/NetworkSamplePlayer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkSamplePlayer : NetworkBehaviour {
+
+	Vector2 destination;
+
+	Camera myCamera;
+
+	NetworkSampleMovement movement;
+
+	void Awake()
+	{
+		movement = GetComponent<NetworkSampleMovement> ();
+		myCamera = Camera.main;
+	}
+
+	[ClientCallback]
+	void Update () {
+
+		if (!isLocalPlayer)
+			return;
+
+		if (Input.GetMouseButtonUp (0)) {
+			Vector2 position = myCamera.ScreenToWorldPoint (Input.mousePosition);
+			CmdMoveTo (position);
+		}
+	}
+		
+	[Command]
+	public void CmdMoveTo(Vector2 position)
+	{
+		movement.MoveTo (position);
+	}
+
+}
